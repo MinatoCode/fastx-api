f65a7c3f151edbec9a010cb737ba92379f29ac38
MinatoCode <minatoofficials08@gmail.com>
2025-09-17 17:06:11 +0545

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+{
+  "name": "collage-api",
+  "version": "1.0.0",
+  "description": "An API that generates a 2x2 collage of images from Dalle-like generator.",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "author": "Your Name",
+  "license": "MIT",
+  "dependencies": {
+    "axios": "^1.7.0",
+    "express": "^4.19.0",
+    "sharp": "^0.33.2"
+  }
+}
